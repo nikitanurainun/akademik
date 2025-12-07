b130f9979a2a1cb67c6689f4405e1fa5bebd550c
nikitanurainun <nikitanurainunn@gmail.com>
2025-12-07 09:16:55 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,10 @@
+<h1> Selamat Datang </h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, quas hic delectus
+     assumenda animi ratione in tempore laborum perspiciatis 
+     nostrum quos cumque pariatur, earum, itaque dolore fugit eaque ad. Earum
+     quam magni, voluptatum itaque cum velit iusto aut deserunt facilis autem perspiciatis quia sit quasi
+     sequi? Tempora excepturi dolor aperiam soluta quasi magnam consectetur tempore minima error velit accusamus 
+     eveniet vel, doloremque dolorum dolores deleniti ullam enim perspiciatis. Quos possimus commodi harum nam assumenda voluptatem, 
+      cupiditate fuga a eligendi dolor sint maiores aut ab qui odio nostrum atque! Esse facere laborum quia ipsum debitis vel dolorem ea possimus adipisci sunt.
+</p>
+
---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "db_akademik";
+
+    $koneksi = new mysqli($host, $user, $password, $db); // Open connection - kunci masuk kesetiap database
+?>
